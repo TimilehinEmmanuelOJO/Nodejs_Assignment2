00292d652a2bc335a72b2e63b589dcbd7d04fd5b
Timilehin OJO <timilehin175@gmail.com>
2023-08-16 20:49:52 +0100

    Adding Pages

--- Ass.js
+++ Ass.js
@@ -1,7 +1,15 @@
 const http = require("http")
 const HOSTNAME = 'localhost'
 const PORT = 3001
+const path = require('path')
 
+
+const indexPath = path.join(__dirname,'index.html' )
+console.log(indexPath)
+//Without using a framework, build a web server to render html files: 
+//When I navigate to “/index.html”, I should see a simple webpage of the student. 
+//(Nothing fancy) Add another feature such that when I navigate to “{random}.html” 
+//it should return with a 404 page
 
 function requestHandler(req, res){
     res.write('Hello this is Timilehin')
